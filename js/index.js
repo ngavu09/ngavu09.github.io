6c7cfe72be5fe27d81e359ef9ad115962fe1e1ff
Jennie Vu <jennievu8991@gmail.com>
2023-09-27 16:40:17 -0400

    adding cursor effects

---
+++ js/index.js
@@ -0,0 +1,30 @@
+// const githubProject1 = document.getElementById("github-project1");
+// const githubProject2 = document.getElementById("github-project2");
+// const githubProject3 = document.getElementById("github-project3");
+// const appDemo1 = document.getElementById("live-demo1");
+// const appDemo2 = document.getElementById("live-demo2");
+// const appDemo3 = document.getElementById("live-demo3");
+
+
+// // Project Github 
+// githubProject1.addEventListener("click", function () {
+//     window.open('https://github.com/Team5SWE/Petit', "_blank");
+// })
+// githubProject2.addEventListener("click", function () {
+//     window.open('https://github.com/jennievu/MobileApp', "_blank");
+// })
+// githubProject3.addEventListener("click", function () {
+//     window.open('https://github.com/Team5SWE/Petit', "_blank");
+// })
+
+
+// // Project Live Demo
+// appDemo1.addEventListener("click", function () {
+//     window.open('https://gilded-jalebi-846bdf.netlify.app/', "_blank");
+// })
+// appDemo2.addEventListener("click", function () {
+//     window.open('https://github.com/Team5SWE/Petit', "_blank");
+// })
+// appDemo3.addEventListener("click", function () {
+//     window.open('https://github.com/Team5SWE/Petit', "_blank");
+// })
